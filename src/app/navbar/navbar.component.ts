77a65bf347d3709d532d3a481c74c4fd0b60de52
Ajetski <45019515+Ajetski@users.noreply.github.com>
2020-10-22 14:17:15 -0400

    #2 add navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
 	selector: 'app-navbar',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-	constructor() { }
+	links = [
+		{ title: 'Home', path: '/home' },
+		{ title: 'Page 2', path: '/page1' },
+		{ title: 'Page 3', path: '/page2' },
+		{ title: 'Page 4', path: '/page3' },
+	];
+
+	constructor(public route: ActivatedRoute) {
+	}
 
 	ngOnInit(): void {
 	}

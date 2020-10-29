710a06dc6093d72645902c3a8da988518d5952e1
Ajetski <45019515+Ajetski@users.noreply.github.com>
2020-10-29 11:10:10 -0400

    #5 add problem list table and navigation

--- src/app/problem/problem-list/problem-list.component.ts
+++ src/app/problem/problem-list/problem-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { prependListener } from 'process';
 
 @Component({
 	selector: 'app-problem-list',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProblemListComponent implements OnInit {
 
-	constructor() { }
+	problems = [
+		{
+			id: 0,
+			name: 'my prob 1',
+			poster: 'jack',
+			description: 'do a thing',
+		},
+		{
+			id: 1,
+			name: 'my prob 2',
+			poster: 'ty',
+			description: 'do 2 things',
+		},
+		{
+			id: 2,
+			name: 'my prob 3',
+			poster: 'john',
+			description: 'do 3 things',
+		}
+	]
+
+	constructor(
+		private router: Router
+	) { }
 
 	ngOnInit(): void {
+	}
+
+	nav(problem: {
+		id: number,
+		name: string,
+		poster: string,
+		description: string
+	}) {
+		this.router.navigate(['problem', 'solve', problem.id])
 	}
 
 }

4cbaba97e48685859d9df051f6ae21a3a266a86d
Ajetski <45019515+Ajetski@users.noreply.github.com>
2020-10-29 14:01:44 -0400

    #3 add angular material dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatListModule } from '@angular/material/list';
+import { MatChipsModule } from '@angular/material/chips';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -12,6 +16,7 @@ import { ProblemComponent } from './problem/problem/problem.component';
 import { ProblemListComponent } from './problem/problem-list/problem-list.component';
 import { ProblemFormComponent } from './problem/problem-form/problem-form.component';
 import { UserHubComponent } from './user-hub/user-hub.component';
+
 
 @NgModule({
 	declarations: [
@@ -27,7 +32,10 @@ import { UserHubComponent } from './user-hub/user-hub.component';
 		BrowserModule,
 		AppRoutingModule,
 		NgbModule,
-		FormsModule
+		FormsModule,
+		BrowserAnimationsModule,
+		MatListModule,
+		MatChipsModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]

6bf944a3db0437b0e777c1f464f0fb3b5c8ddc7f
Ajetski <45019515+Ajetski@users.noreply.github.com>
2020-11-04 16:47:45 -0500

    #6 cache content for user

--- src/app/problem/problem/problem.component.ts
+++ src/app/problem/problem/problem.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 
 import * as ace from 'ace-builds'; // ace module ..
 import 'ace-builds/webpack-resolver';
@@ -7,8 +7,8 @@ import 'ace-builds/src-min-noconflict/mode-javascript';
 // ui-theme package
 import 'ace-builds/src-min-noconflict/theme-twilight';
 import { Problem, ProblemService } from 'src/app/services/problem.service';
-import { HttpClientModule } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
+import { interval, Subscription } from 'rxjs';
 
 const THEME = 'ace/theme/twilight';
 const LANG = 'ace/mode/javascript';
@@ -18,9 +18,10 @@ const LANG = 'ace/mode/javascript';
 	templateUrl: './problem.component.html',
 	styleUrls: ['./problem.component.scss']
 })
-export class ProblemComponent implements OnInit {
+export class ProblemComponent implements OnInit, OnDestroy {
 	codeEditor: ace.Ace.Editor;
 	problem: Problem;
+	saveSubscription: Subscription;
 
 	constructor(
 		private problemService: ProblemService,
@@ -28,16 +29,6 @@ export class ProblemComponent implements OnInit {
 	) { }
 
 	ngOnInit() {
-		//get problem data from DB
-		this.route.params.subscribe(params => {
-			if (params['id'] != null)
-				this.problemService.getProblemById(params['id']).subscribe(
-					problem => {
-						this.problem = problem;
-					}
-				);
-		});
-
 		//init the code editor
 		const editorOptions: Partial<ace.Ace.EditorOptions> = {
 			highlightActiveLine: true,
@@ -48,6 +39,34 @@ export class ProblemComponent implements OnInit {
 		this.codeEditor.setTheme(THEME);
 		this.codeEditor.getSession().setMode(LANG);
 		this.codeEditor.setShowFoldWidgets(true);
+
+		//get problem data from DB
+		this.route.params.subscribe(params => {
+			if (params['id'] != null)
+				this.problemService.getProblemById(params['id']).subscribe(
+					problem => {
+						this.problem = problem;
+
+						// Load saved code if it exists
+						const oldCode = localStorage.getItem(`code_${problem.id}`);
+						if (oldCode != null) {
+							this.codeEditor.setValue(oldCode);
+						}
+
+						// Save new code whenever use inputs it
+						this.saveSubscription = interval(1000).subscribe(() => {
+							localStorage.setItem(
+								`code_${problem.id}`,
+								this.getCode());
+						});
+					}
+				);
+		});
+	}
+
+	ngOnDestroy() {
+		// Stop listenting to user code when they leave page
+		this.saveSubscription.unsubscribe();
 	}
 
 	getCode() {

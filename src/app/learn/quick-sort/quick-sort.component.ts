0b533f901585d5ff1d5584d4084751b6f07353b8
tyjdor <tyjdor96@gmail.com>
2020-11-06 20:53:07 -0500

    #10 init quick sort component

---
+++ src/app/learn/quick-sort/quick-sort.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-quick-sort',
+	templateUrl: './quick-sort.component.html',
+	styleUrls: ['./quick-sort.component.scss']
+})
+export class QuickSortComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+}

2fa58f6fa073855607ba0986a06b0915abedf79c
tyjdor <tyjdor96@gmail.com>
2020-11-06 20:52:19 -0500

    #10 init insertion sort component

---
+++ src/app/learn/insertion-sort/insertion-sort.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-insertion-sort',
+	templateUrl: './insertion-sort.component.html',
+	styleUrls: ['./insertion-sort.component.scss']
+})
+export class InsertionSortComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+}

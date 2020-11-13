b15eccee6b8402ee46878f0e17314e9d0013c383
Ajetski <45019515+Ajetski@users.noreply.github.com>
2020-11-13 01:06:40 -0500

    #7 make interfaces and api calls for problem, testcase, and param

--- src/app/services/problem.service.ts
+++ src/app/services/problem.service.ts
@@ -11,6 +11,20 @@ export interface Problem {
 	// categories?: string[]
 }
 
+export interface Parameter {
+	paramId: number,
+	name: String,
+	problem: number
+}
+
+export interface TestCase {
+	id: number,
+	problem: number,
+	public: boolean,
+	sampleInput: String,
+	sampleOutput: String
+}
+
 @Injectable({
 	providedIn: 'root'
 })
@@ -26,5 +40,37 @@ export class ProblemService {
 
 	getProblemById(id: number) {
 		return this.http.get<Problem>(`${environment.apiBase}/problem/${id}`);
+	}
+
+	getParamsByProblemId(id: number) {
+		return this.http.get<Parameter[]>(`${environment.apiBase}/parameter/search?problem=${id}`);
+	}
+
+	getTestCasesByProblemId(id: number) {
+		return this.http.get<TestCase[]>(`${environment.apiBase}/testcase/search?problem=${id}`);
+	}
+
+	createProblem(problem: Problem) {
+		return this.http.post<Problem>(`${environment.apiBase}/problem`, problem);
+	}
+
+	createParameter(param: Parameter) {
+		return this.http.post<Parameter>(`${environment.apiBase}/parameter`, param);
+	}
+
+	createTestCase(testCase: TestCase) {
+		return this.http.post<TestCase>(`${environment.apiBase}/testcase`, testCase);
+	}
+
+	editProblem(problem: Problem) {
+		return this.http.put<Problem>(`${environment.apiBase}/problem/${problem.id}`, problem);
+	}
+
+	editParameter(param: Parameter) {
+		return this.http.put<Parameter>(`${environment.apiBase}/parameter/${param.paramId}`, param);
+	}
+
+	editTestCase(testCase: TestCase) {
+		return this.http.put<TestCase>(`${environment.apiBase}/testcase/${testCase.id}`, testCase);
 	}
 }

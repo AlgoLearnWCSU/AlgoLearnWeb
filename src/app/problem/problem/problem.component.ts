d94d301cbb33208cba1ae53827f5b9c28881ba6c
Caleb Garrick <caleb.garrick@gmail.com>
2020-11-15 21:04:39 -0500

    #6 fix parmid assignment

--- src/app/problem/problem/problem.component.ts
+++ src/app/problem/problem/problem.component.ts
@@ -1,3 +1,4 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Parameter, Problem, ProblemService } from 'src/app/services/problem.service';
@@ -7,7 +8,7 @@ import { Parameter, Problem, ProblemService } from 'src/app/services/problem.ser
 	templateUrl: './problem.component.html',
 	styleUrls: ['./problem.component.scss']
 })
-export class ProblemComponent {
+export class ProblemComponent implements OnInit {
 	problem: Problem;
 	saveId: string;
 	params: Parameter[];
@@ -18,18 +19,18 @@ export class ProblemComponent {
 	) { }
 
 	ngOnInit() {
-		//get problem data from DB
+		// get problem data from DB
 		this.route.params.subscribe(params => {
 			if (params.id != null) {
-				this.saveId
+				this.saveId = params.id;
 				this.problemService.getProblemById(params.id).subscribe(
 					problem => {
 						this.problem = problem;
 					}
 				);
 				this.problemService.getParamsByProblemId(params.id).subscribe(
-					params => {
-						this.params = params;
+					parameters => {
+						this.params = parameters;
 					}
 				);
 			}

b7348f277629815e09446e8f75874dd9479a7c5d
Ajetski <45019515+Ajetski@users.noreply.github.com>
2020-11-25 18:08:27 -0500

    #6 add solution service

---
+++ src/app/services/solution.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+export interface Solution {
+
+}
+
+export interface Result {
+	stdout?: string;
+	time?: string;
+	memory?: number;
+	stderr?: string;
+	token?: string;
+	status?: {
+		id?: number;
+		description?: string;
+	};
+}
+
+@Injectable({
+	providedIn: 'root'
+})
+export class SolutionService {
+
+	constructor(
+		private http: HttpClient
+	) { }
+
+	getSolutionById(id: number) {
+		return this.http.get<Solution>(`${environment.apiBase}/solution/${id}`);
+	}
+
+	getSolutionsByUsernameAndProblem(id: number) {
+		return this.http.get<Solution[]>(`${environment.apiBase}/solution/${id}`);
+	}
+
+	getResultsBySolutionId(id: number) {
+		return this.http.get<{ submissions: Result[] }>(`${environment.apiBase}/solution/result/${id}`);
+	}
+}

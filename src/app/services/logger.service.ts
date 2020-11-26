de3ae3811b0a55ed0252da374af5baa42a0f743d
Ajetski <45019515+Ajetski@users.noreply.github.com>
2020-11-25 20:18:54 -0500

    #6 update logger service to prevent content overflow

--- src/app/services/logger.service.ts
+++ src/app/services/logger.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
 import { UserService } from './user.service';
 
 @Injectable({
@@ -13,11 +12,14 @@ export class LoggerService {
 		private userService: UserService
 	) { }
 
-	log(message: string) {
-		this.http.post('https://discord.com/api/webhooks/780613046543450123/zs3ByIBeC3W36Etwm3bJ_6RhCADgZ9g1jR53U0Zo5KTAO0aIyEtooMoLjc7RzyhlTDcx', {
+	logError(message: string) {
+		if (message.length > 2000) {
+			message = message.substr(0, 1997) + '```';
+		}
+		return this.http.post('https://discord.com/api/webhooks/780613046543450123/zs3ByIBeC3W36Etwm3bJ_6RhCADgZ9g1jR53U0Zo5KTAO0aIyEtooMoLjc7RzyhlTDcx', {
 			content: message,
 			username: this.userService.username
-		}).subscribe(() => { }, err => console.error('Couldn\'t log: ', err));
+		});
 	}
 
 }

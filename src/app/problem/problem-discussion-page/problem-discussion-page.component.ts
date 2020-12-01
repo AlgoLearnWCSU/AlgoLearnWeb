ec08dae745b8934c3482881f7d70a52abf43f6ac
Caleb Garrick <caleb.garrick@gmail.com>
2020-12-01 15:53:20 -0500

    #13 display problem on discussion page, fix link and icon

--- src/app/problem/problem-discussion-page/problem-discussion-page.component.ts
+++ src/app/problem/problem-discussion-page/problem-discussion-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Category, Problem, ProblemService } from 'src/app/services/problem.service';
 
 @Component({
 	selector: 'app-problem-discussion-page',
@@ -10,14 +11,30 @@ export class ProblemDiscussionPageComponent implements OnInit {
 
 	@Input() id: number;
 
+	categories: Category[];
+	problem: Problem;
+
 	constructor(
-		private route: ActivatedRoute
+		private route: ActivatedRoute,
+		private problemService: ProblemService
 	) { }
 
 	ngOnInit(): void {
 		if (!this.id) {
 			this.route.params.subscribe(
-				params => this.id = params.id,
+				params => {
+					this.id = params.id;
+					this.problemService.getProblemById(params.id).subscribe(
+						problem => {
+							this.problem = problem;
+						}
+					);
+					this.problemService.getCategoriesByProblemId(params.id).subscribe(
+						categories => {
+							this.categories = categories;
+						}
+					);
+				},
 				err => console.error(err)
 			);
 		}
